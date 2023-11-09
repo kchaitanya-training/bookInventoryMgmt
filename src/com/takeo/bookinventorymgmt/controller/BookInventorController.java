cd99fbecfbcf83ea47face591715883ad2fa78d9
krishna chaitanya <kchaitanya.48@outlook.com>
2023-11-10 01:33:48 +0530

    fixex initialization step

--- src/com/takeo/bookinventorymgmt/controller/BookInventorController.java
+++ src/com/takeo/bookinventorymgmt/controller/BookInventorController.java
@@ -1,6 +1,7 @@
 package com.takeo.bookinventorymgmt.controller;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import com.takeo.bookinventorymgmt.exception.BookInventoryException;
@@ -19,12 +20,41 @@ public class BookInventorController {
 		InputStreamReader reader=new InputStreamReader(System.in);
 		BufferedReader br=new BufferedReader(reader);
 		
-			
-	
-				try {
-					service.addBook(br);
+		
+			try {
+				String str = br.readLine();
+				System.out.print("read -1 :");
+				System.out.print("update -2 :");
+				System.out.print("delete -3 :");
+				System.out.print("insert  - 4 :");
+				String s=br.readLine();
+						
+				int val = Integer.parseInt(s);
+				switch (val) {
+				  case 1:
+				    System.out.println("read");
+				 
+				    break;
+				  case 2:
+				    System.out.println("update");
+				    break;
+				  case 3:
+				    System.out.println("delete");
+				    break;
+				  case 4:
+				    System.out.println("insert");
+				    service.addBook(br);
+				    break;
+				  
+				}
+				
+				
+					
 				} catch (BookInventoryException e) {
 					System.out.println(e.getMessage());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 		
 		
